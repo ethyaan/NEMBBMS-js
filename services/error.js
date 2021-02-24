ee34cbb8243062d617e6b37c3dfd0b8c82f175b5
Ehsan Aghaei <ehsan.aghaei@tecnotree.com>
2021-02-24 13:33:34 +0330

    add error service

---
+++ services/error.js
@@ -0,0 +1,25 @@
+import logger from './logger';
+
+const createErrorObject = (options, additionalInfo) => {
+	const additionalInformation = { message: options.msg };
+	Object.assign(additionalInformation, additionalInfo || {});
+	return {
+		responseCode: options.statusCode || 422,
+		errorObj: {
+			errorCode: options.errorCode || 'GeneralError',
+			additionalInformation
+		}
+	};
+};
+
+const createError = (options, additionalInfo) => {
+	return Promise.reject(createErrorObject(options, additionalInfo));
+};
+
+const handleError = (error, res) => {
+	const errorOutput = error.errorObj || createErrorObject({ msg: 'Error while performing your request' }).errorObj;
+	logger.error('Catch Handler => ', error);
+	res.status(error.responseCode || 500).send(errorOutput);
+};
+
+module.exports = { createErrorObject, createError, handleError };
