2b9ec327a2971fcd6c6782ed43533222413b553d
Ehsan Aghaei <ehsan.aghaei@tecnotree.com>
2021-02-24 13:25:21 +0330

    implement model methods interface

--- utils/model.factory.js
+++ utils/model.factory.js
@@ -18,6 +18,72 @@ class MasterModel {
 		return this.model;
 	}
 
+    /**
+	 * get Entity details from params and insert into the DB, return Object inside the Promise
+	 * @param EntityObject
+	 */
+    async createEntity(EntityObject) {
+
+    }
+
+    /**
+	 * find Entity by specified query
+	 * @param query
+	 * @param projection
+	 * @param options
+	 * @param find
+	 */
+    async findEntityByParams(query, projection, options, find) {
+
+    }
+
+    /**
+	 * find entity with pagination required parameters
+	 * @param query
+	 * @param projection
+	 * @param options
+	 * @param pagination
+	 */
+    async findEntityByParamsPagination(query, projection, options, pagination) {
+
+    }
+
+    /**
+	 * update Entity info by previous model
+	 * @param model
+	 * @param properties
+	 */
+	async updateEntityByModel(model, properties) {
+
+    }
+
+    /**
+	 * delete one single document
+	 * @param query
+	 * @param options
+	 */
+	async deleteEntity(query, options) {
+
+    }
+
+    /**
+	 * parse mongoose Error Object and extract information
+	 * @param err
+	 */
+	mongooseErrorHandler (err) {
+		const response = {};
+		if (!err) {
+			return err;
+		} else if (err.errors) {
+			Object.keys(err.errors).forEach((item) => {
+				response[item] = err.errors[item].message;
+			});
+			return response;
+		} else {
+			return err.toString();
+		}
+	}
+
 }
 
 module.exports = MasterModel;
