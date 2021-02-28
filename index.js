7cb5fb28d55193db4efcf702d839afbad6538fbb
Ehsan Aghaei <ehsan.aghaeii@gmail.com>
2021-02-28 10:27:00 +0330

    enable mongodb connection

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const config            = require('./config');
 const logger            = require('./services/logger');
 const app               = require('./app');
 
-const mongoHost = new url.URL(config.MONGODB_URI).host;
 const mongoPassword = encodeURIComponent(config.MONGODB_PASSWORD);
 const mongoDBConnectionURI = config.MONGODB_URI.replace('_pwd_', mongoPassword);
+const mongoHost = new url.URL(mongoDBConnectionURI).host;
 
 const connectMongoDB = () => {
 
@@ -26,21 +26,20 @@ const connectMongoDB = () => {
 	};
 
 	return new Promise((resolve, reject) => {
-        return resolve(true);
-		// mongoose.connect(mongoDBConnectionURI, connectionOptions, async (err) => {
-		// 	if (err) {
-		// 		logger.error('Error connecting mongoDB => ', err);
-		// 		return reject(true);
-		// 	}
-		// 	logger.success(`Connected to mongoDB at ${mongoHost}`);
-		// 	return resolve(true);
-		// });
+		mongoose.connect(mongoDBConnectionURI, connectionOptions, async (err) => {
+			if (err) {
+				logger.error('Error connecting mongoDB => ', err);
+				return reject(true);
+			}
+			logger.success(`Connected to mongoDB at ${mongoHost}`);
+			return resolve(true);
+		});
 	});
 };
 
 const startServer = async () => {
 	try {
-		// await connectMongoDB();
+		await connectMongoDB();
 		// await app._Auth.connect();
 		app.listen(config.PORT, () => {
 			console.log(`App listening on port ${config.PORT}`);
