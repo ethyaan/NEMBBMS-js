4126c65226188eefad21ef785acb79f77610de45
Ethyaan <ehsan.aghaeii@gmail.com>
2023-04-21 02:17:59 +0300

    commenting user functionality

--- modules/user/route.js
+++ modules/user/route.js
@@ -1,5 +1,5 @@
-const router = require('express').Router;
-const validator = require('./validator');
+import { Router } from 'express';
+// const validator = require('./validator');
 
 /**
  * Define users end API Routes
@@ -12,20 +12,20 @@ class usersRouter {
 	 */
 	constructor(app) {
 
-		const controller = require('./controller')(app);
-		const userRouter = router();
+		// 		const controller = require('./controller')(app);
+		const userRouter = Router();
 
-		userRouter.post('/', validator.signup(), validator.validate, controller.signup);
-		userRouter.post('/resendVerification', validator.resendVerification(), validator.validate, controller.resendVerification);
-		userRouter.post('/verify', validator.verify(), validator.validate, controller.verify);
-		userRouter.post('/setProfile', validator.setProfile(), validator.validate, controller.setProfile);
-		userRouter.post('/login', validator.login(), validator.validate, controller.userAuth, controller.login);
-		userRouter.post('/logout', [app._Auth.isUserLoggedIn, app._Auth.logOutUser], controller.logout);
-		userRouter.post('/changePassword', app._Auth.isUserLoggedIn, validator.changePassword(), validator.validate, controller.changeUserPassword);
-		userRouter.post('/updateProfile', app._Auth.isUserLoggedIn, validator.updateProfile(), validator.validate, controller.updateProfile);
-		userRouter.post('/forgetPassword', validator.forgetPassword(), validator.validate, controller.forgetPassword);
-		userRouter.post('/setNewPassword', validator.setNewPassword(), validator.validate, controller.setNewPassword);
-		userRouter.get('/getProfile', [app._Auth.isUserLoggedIn], controller.getProfile);
+		// 		userRouter.post('/', validator.signup(), validator.validate, controller.signup);
+		// 		userRouter.post('/resendVerification', validator.resendVerification(), validator.validate, controller.resendVerification);
+		// 		userRouter.post('/verify', validator.verify(), validator.validate, controller.verify);
+		// 		userRouter.post('/setProfile', validator.setProfile(), validator.validate, controller.setProfile);
+		// 		userRouter.post('/login', validator.login(), validator.validate, controller.userAuth, controller.login);
+		// 		userRouter.post('/logout', [app._Auth.isUserLoggedIn, app._Auth.logOutUser], controller.logout);
+		// 		userRouter.post('/changePassword', app._Auth.isUserLoggedIn, validator.changePassword(), validator.validate, controller.changeUserPassword);
+		// 		userRouter.post('/updateProfile', app._Auth.isUserLoggedIn, validator.updateProfile(), validator.validate, controller.updateProfile);
+		// 		userRouter.post('/forgetPassword', validator.forgetPassword(), validator.validate, controller.forgetPassword);
+		// 		userRouter.post('/setNewPassword', validator.setNewPassword(), validator.validate, controller.setNewPassword);
+		// 		userRouter.get('/getProfile', [app._Auth.isUserLoggedIn], controller.getProfile);
 
 		app.use('/user', userRouter);
 
@@ -33,4 +33,4 @@ class usersRouter {
 
 }
 
-module.exports = (app) => new usersRouter(app);
+export default (app) => new usersRouter(app);
