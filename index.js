d6d207ec788207256ff1b1d54a378f3774342c50
Ethyaan <ehsan.aghaeii@gmail.com>
2023-04-22 12:36:44 +0300

    fix: update config problem

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import path from 'path';
-import * as dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '.env') });
 import mongoose from 'mongoose';
 import * as url from 'url';
 import config from './config.js';
