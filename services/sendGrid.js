6fa1eeb89aba6c5779e7f5eb4f37079bc30d507f
AmirAliEidivandi <amirah648887@gmail.com>
2023-05-11 00:33:27 +0330

    fix: refactor file path logic to use node built-in modules

--- services/sendGrid.js
+++ services/sendGrid.js
@@ -1,9 +1,11 @@
 import fs from "fs";
 import path from "path";
-import logger from "./logger";
+import { Logger } from "../common/index.js";
 import sgMail from "@sendgrid/mail";
-import config from "../config";
-const filePath = path.join(__dirname, "../email-templates");
+import config from "../config.js";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const filePath = dirname(fileURLToPath(import.meta.url));
 
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 
@@ -25,9 +27,9 @@ class SendGrid {
 
     try {
       await sgMail.sendMultiple(msg);
-      logger.success("Mail Sent To :", emailAddressList);
+      Logger.success("Mail Sent To :", emailAddressList);
     } catch (error) {
-      logger.error("Error In Sending Mail", error);
+      Logger.error("Error In Sending Mail", error);
     }
   };
 
@@ -60,9 +62,9 @@ class SendGrid {
       });
       this.sendMail(subject, template, emailAddressList, from);
     } catch (e) {
-      logger.error("Error in sending e-mail By Template", e.toString());
+      Logger.error("Error in sending e-mail By Template", e.toString());
     }
   };
 }
 
-export default (app) => new SendGrid(app);
+export default () => new SendGrid();
